49e7ce439008d57ae182025558047185432d3ba8
kavinkumar06 <kavinm09@gmail.com>
2017-07-14 12:23:14 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+# Multiplication
+import java.io.*;
+import java.util.*;
+public class Multiplication
+{
+  public static void main(String args[])
+  {
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
